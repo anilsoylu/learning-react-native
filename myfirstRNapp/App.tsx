82d675c51c2f1b6a55809badc2ead2267260b971
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2023-05-04 13:52:28 +0300

    Platform

--- myfirstRNapp/App.tsx
+++ myfirstRNapp/App.tsx
@@ -1,71 +1,13 @@
-import React, {useState, useEffect} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  Image,
-  TouchableOpacity,
-  RefreshControl,
-  Alert,
-  Dimensions,
-} from 'react-native';
+import React from 'react';
+import {Platform, Text, SafeAreaView, StyleSheet} from 'react-native';
 
 export default function App() {
-  const [photos, setPhotos] = useState([]);
-  const [refreshing, setRefreshing] = useState(false);
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/photos')
-      .then(response => response.json())
-      .then(json => setPhotos(json))
-      .catch(error => console.error(error));
-  }, []);
-
-  const onRefresh = () => {
-    setRefreshing(true);
-    fetch('https://jsonplaceholder.typicode.com/photos')
-      .then(response => response.json())
-      .then(json => setPhotos(json))
-      .catch(error => console.error(error))
-      .finally(() => setRefreshing(false));
-  };
-
-  const renderItem = ({item}) => (
-    <TouchableOpacity onPress={() => handlePress(item)}>
-      <View style={styles.photo}>
-        <Image
-          source={{uri: item.url}}
-          style={styles.image}
-          resizeMode="contain"
-        />
-
-        <Text style={styles.title} numberOfLines={1}>
-          {item.title}
-        </Text>
-      </View>
-    </TouchableOpacity>
-  );
-
-  const handlePress = item => {
-    Alert.alert(
-      'Photo Info',
-      `Title: ${item.title}\nAlbum ID: ${item.albumId}`,
-    );
-  };
-
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={photos}
-        renderItem={renderItem}
-        keyExtractor={item => item.id.toString()}
-        contentContainerStyle={styles.list}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-        }
-      />
-    </View>
+    <SafeAreaView>
+      <Text style={[styles.text, styles.title]}>
+        This is an {Platform.OS === 'ios' ? 'iOS' : 'Android'} app!
+      </Text>
+    </SafeAreaView>
   );
 }
 
@@ -76,28 +18,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  list: {
-    padding: 10,
-  },
-  photo: {
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 10,
-    marginVertical: 5,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  image: {
-    width: Dimensions.get('window').width - 40,
-    height: Dimensions.get('window').height / 3,
-    borderRadius: 10,
-    marginBottom: 10,
+  text: {
+    color: Platform.OS === 'ios' ? 'blue' : 'green',
   },
   title: {
     fontSize: 14,
