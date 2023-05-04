b9b2b278b112c06f24890b67a3e383d675e2c7ab
Anıl Soylu <56033298+anilsoylu@users.noreply.github.com>
2023-05-04 11:24:50 +0300

    Text

--- myfirstRNapp/App.tsx
+++ myfirstRNapp/App.tsx
@@ -6,14 +6,40 @@
  */
 
 import React from 'react';
-import {Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello, World!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>
+        Welcome to <Text style={styles.customText}>React Native</Text>
+        {'\n'}
+        TypeScript Starter!{'\n'}
+      </Text>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+
+  text: {
+    fontSize: 16,
+    color: 'red',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+
+  customText: {
+    fontSize: 16,
+    color: 'yellow',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    backgroundColor: 'red',
+  },
+});
 
 export default App;
